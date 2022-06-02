096fc3c59b8e276e37788ff4fe0cfd939a7d1821
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-02 14:06:55 -0700

    my personal work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - my entry point 
-* Description:and that piece of art is useful - Dora korpar, 2015-10-19
-* Return: 1
-*/ 
+* main - print quote
+* Description: prints Dora's quote
+* Return: Always 1 (Success)
+*/
 
 int main(void)
-{ 
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
