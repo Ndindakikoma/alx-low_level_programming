bc8373b1cb5223011a750cd0ed11111134043652
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-02 06:42:07 -0700

    my personal work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,20 @@
 
 
 
-int main(void)
+/**
 
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
 {
 
-	puts("Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,41 @@
 
 
 
-int main(void)
+/**
 
-{
+ * main - Entry point
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+ *
 
-	return (0);
+ * Return: Always 0 (Success)
 
-}
+ */
+int main(void)                                                                                                                                           
+
+                                                                                                                                                         
+
+{                                                                                                                                                        
+
+                                                                                                                                                         
+
+        printf("with proper grammar, but the outcome is a piece of art,\n");                                                                             
+
+                                                                                                                                                         
+
+        return (0);                                                                                                                                      
+
+                                                                                                                                                         
+
+}                
+
+
+
+
+
+
+
+
+
+
+
+
