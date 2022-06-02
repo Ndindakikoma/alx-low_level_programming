7d61ca8c5c44acc1b953aa917e305605df98d45a
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-02 06:03:37 -0700

    my personal work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#include <stdio.h> 
+#include <stdio.h>
+
+
 
 int main(void)
+
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+
 }
