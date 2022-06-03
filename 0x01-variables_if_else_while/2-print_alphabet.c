578d83764028c0144eac1c80fa99a777d919e72b
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-03 12:53:40 -0700

    my personal work

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+/*
+* File: 2-print_alphabet.c
+* Auth: Maureein K Ndinda
 */
 
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-	char low;
-	
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
 
 	return (0);
 }
