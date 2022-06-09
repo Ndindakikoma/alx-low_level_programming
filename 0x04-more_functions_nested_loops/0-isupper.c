6ec3bb4fab74b15c3184048dbbdb897f8770a038
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-09 14:41:21 -0700

    My personal work

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,19 @@
-/*
-* File: 0-isupper.c
-* Auth: Maureen Ndinda
-*/
-
 #include "holberton.h"
+#include <stdio.h>
 
 /**
-* _isupper - Checks for uppercase characters.
-* @c:The character to be checked.
-*
-* Return: 1 if character is uppercase, 0 otherwise.
+* _isupper - checks for uppercase character
+* @c:Variable text
+* Return: Always 0.
 */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
-
+	{
+	return (1);
+	}
 	else
-		return (0);
+	{
+	return (0);
+	}
 }
