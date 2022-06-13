659a924c2f2e45b13f432a423d7f682fa700ab0b
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-13 09:05:20 -0700

    my personal work

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-* reset_to_98 - main
-* @n: - pinter to int
+* reset_to_98 - updates the value *n points to
+* @n: the number to modify.
 */
 
 void reset_to_98(int *n)
 {
-	int *num;
-
-	int x = 98;
-
-	num = &x;
-	*n = *num;
+	*n = 98;
 }
