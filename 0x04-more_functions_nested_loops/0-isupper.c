44efaf1c69223da36b7c042a19bb4c718c21648e
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-15 11:48:03 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,11 @@
 
 /**
 * _isupper - checks for uppercase character
-* @c:Variable text
+* @c: Variable text
 * Return: Always 0.
 */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-	return (1);
-	}
-	else
-	{
-	return (0);
-	}
-}
