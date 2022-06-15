c9cee75d3e5a52b2395e23ce5835a2acfc765a0d
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-15 07:58:52 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,19 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else if (n < 0)
-		_putchar('-');
-		return (-1);
-	}
-	return (0);
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+_putchar('-');
+return (-1);
+}
+return (0);
 }
