c71f69384ceb056d616c5aa47f1ac14e888f9da4
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-15 03:55:47 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,24 @@
-#ifndef main
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+* File: main.h
+*
+* Desc: Header file containing declartions for all function
+*/
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_teble(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
 
-#define main
 #endif
