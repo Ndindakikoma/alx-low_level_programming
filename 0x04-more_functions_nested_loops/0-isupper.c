bfe13f322b6c896e080bf5d884a506d9a53dd7b7
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-15 11:52:49 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
+* _isupper - Check if paramter c represents an uppercase letter.
+* @c: Int representing a character
 * Return: Always 0.
 */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
+		return (1);
+	}
+	return (0);
+}
