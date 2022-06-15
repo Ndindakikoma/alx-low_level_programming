3f0727f3fa5ccf6c26c8a45cc1b5a3e6237676cf
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-15 05:36:26 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,16 @@ void print_alphabet_x10(void)
 {
 	int b = 0;
 	while (b < 10)
+
 	{
-	char a = 'a';
-	while (a <= 'z')
-		{
-		_putchar(a);
-		a++;
-		}
-	_putchar('\n');
-	b++;
+		char a = 'a';
+		while (a <= 'z')
+
+			{
+				_putchar(a);
+				a++;
+			}
+		_putchar('\n');
+		b++;
 	}
 }
