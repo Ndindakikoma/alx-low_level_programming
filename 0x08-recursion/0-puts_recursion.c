2655d6cdbc8d12cfbc28d5ec48126b53e594ec17
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-22 10:12:27 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts_recursion - print a string.
+* @s: pointer to char
+*Return: No.
+**/
+
+void _puts_recursion(char *s);
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
