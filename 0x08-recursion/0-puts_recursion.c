c5b29da02cf1bb202a7c483fa4d69150daf73c28
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-22 10:24:57 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * _puts_recursion - print a string.
@@ -11,7 +10,7 @@ void _puts_recursion(char *s);
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
+		_putchar(*s)
 		_puts_recursion(s + 1);
 	}
 	else
