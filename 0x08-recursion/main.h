6705899d54e84b3123ccbe20f07ee43f8806630e
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-22 10:36:38 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,27 +1,28 @@
-#ifndef FILE_MAIN                                                                                                                                        
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#define FILE_MAIN                                                                                                                                        
+void _puts_recursion(char *s);
 
-                                                                                                                                                         
+void _print_rev_recursion(char *s);
 
-void _puts_recursion(char *s);                                                                                                                           
+void _putchar(char c);
 
-void _print_rev_recursion(char *s);                                                                                                                      
+int _strlen_recursion(char *s);
 
-int _strlen_recursion(char *s);                                                                                                                          
+int factorial(int n);
 
-int factorial(int n);                                                                                                                                    
+int _pow_recursion(int x, int y);
 
-int _pow_recursion(int x, int y);                                                                                                                        
+int _sqrt_recursion(int n);
 
-int _sqrt_recursion(int n);                                                                                                                              
+int _sqrt(int n, int i);
 
-int is_prime_number(int n);                                                                                                                              
+int wildcmp(char *s1, char *s2);
 
-int is_palindrome(char *s);                                                                                                                              
+int is_prime_number(int n);
 
-int wildcmp(char *s1, char *s2);                                                                                                                         
+int is_palindrome(char *s);
 
-                                                                                                                                                         
 
-#endif 
+
+#endif
