01935ba41928ed063bd3574b1d0da559e2205cd4
Ndindakikoma <ndindakikoma@gmail.com>
2022-06-22 11:02:27 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,3 +1,16 @@
 #include "main.h"
 
-* _puts_recursion - print a string.
+/**
+* _print_rev_recursion - reverse
+* @s: pointer to string params
+*
+**/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
