f3b1ec7956c87b9ceecd15bf0d09bc4b9ac958b3
Ndindakikoma <ndindakikoma@gmail.com>
2022-07-22 02:20:37 +0300

    Hellow world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main()
 {
-	puts("programming is like buildng a multilungual puzzle \n"
-	return 0;
+        puts("programming is like buildng a multilungual puzzle \n"
+        return 0;
 }
