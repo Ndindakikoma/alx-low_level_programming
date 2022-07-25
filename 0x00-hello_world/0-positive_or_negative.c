2e41ce6e404ffeb6b8bcc02cf9153f480d39fb8d
Ndindakikoma <ndindakikoma@gmail.com>
2022-07-26 02:48:59 +0300

    variables ,if, else, while

---
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+*assigns random number
+*
+**/
+
+int main(void)
+{
+  int n;
+
+  if(n>0){
+    printf("positive");
+  }
+    else if (n==0){
+      printf("is zero");
+    }
+    else{
+      printf("is negative");
+    }
+}
