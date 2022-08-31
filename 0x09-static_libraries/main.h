cb7a16736c67082eaad5bfff060f08dfc24d6793
Ndindakikoma <ndindakikoma@gmail.com>
2022-08-31 20:45:44 +0300

    Static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,26 +1,27 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-int _isupper(int c);
-char *_memset(char *s, char b, unsigned int n);
-char *_strcat(char *dest, char *src);
-int _atoi(char *s);
-int _isdigit(int c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strchr(char *s, char c);
-int _strlen(char *s);
-char *_strncpy(char *dest, char *src, int n);
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+/*copy list from task*/
+
+int _putchar(char c);
 int _islower(int c);
-void _puts(char *str);
-int _strcmp(char *s1, char *s2);
-unsigned int _strspn(char *s, char *accept);
 int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _abs(int c);
-char *_strcpy(char *dest, char *src);
-int _putchar(char c);
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+
 #endif
