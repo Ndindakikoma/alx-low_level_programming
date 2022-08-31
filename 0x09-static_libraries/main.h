97e42d4008cd08b5d19f5f84d4911634ee065f05
Ndindakikoma <ndindakikoma@gmail.com>
2022-08-31 20:09:32 +0300

    static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,25 +1,26 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _isupper(int c);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+int _isdigit(int c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strchr(char *s, char c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
 unsigned int _strspn(char *s, char *accept);
+int _isalpha(int c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+int _abs(int c);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #endif
